0460b8d5bf9f44fc4488713756af90aa83a56f64
Adilson Bacelar <a.m.bacelar@gmail.com>
2022-04-19 19:23:24 +0100

    audit packages add 2 materials updated brands

--- utils.js
+++ utils.js
@@ -5,8 +5,8 @@ const getMaterialsFromString = (inputString) => {
       materials.push(material.Name);
     }
   });
-  const TencelIndex = materials.indexOf('Tencel');
-  const lyocellIndex = materials.indexOf('Lyocell');
+  const TencelIndex = materials.indexOf("Tencel");
+  const lyocellIndex = materials.indexOf("Lyocell");
   const hasLyocell = lyocellIndex !== -1;
   const hasTencel = TencelIndex !== -1;
   if (hasLyocell && hasTencel) {
@@ -17,244 +17,258 @@ const getMaterialsFromString = (inputString) => {
 
 const matcharacteristic = [
   {
-    Name: 'Acrylic',
+    Name: "Acrylic",
     Characteristic:
-      'is lightweight, soft, and warm - similar to wool but it is machine washable.',
+      "is lightweight, soft, and warm - similar to wool but it is machine washable.",
   },
   {
-    Name: 'Canvas',
+    Name: "Canvas",
     Characteristic:
       "is durable, sturdy, and heavy duty. It's great for the outdoors.",
   },
   {
-    Name: 'Cashmere',
+    Name: "Cashmere",
     Characteristic:
       "is lighter and softer than sheep's wool but is much warmer.",
   },
   {
-    Name: 'Chiffon',
-    Characteristic: 'is sheer and lightweight and normally looks translucent.',
+    Name: "Chiffon",
+    Characteristic: "is sheer and lightweight and normally looks translucent.",
   },
   {
-    Name: 'Cotton',
-    Characteristic: 'is soft, breathable, lightweight, and durable.',
+    Name: "Cotton",
+    Characteristic: "is soft, breathable, lightweight, and durable.",
   },
   {
-    Name: 'Elastane',
+    Name: "Crepe",
+    Characteristic: "is usually a delicate form of wool, silk, or polyester.",
+  },
+  {
+    Name: "Elastane",
     Characteristic:
       "is elastic. It's blended with other fabric types to make them stretchy.",
   },
   {
-    Name: 'Jersey',
-    Characteristic: 'is a soft stretchy, knit fabric.',
+    Name: "Jersey",
+    Characteristic: "is a soft stretchy, knit fabric.",
   },
   {
-    Name: 'Leather',
-    Characteristic: 'is a strong, durable, and wrinkle-resistant fabric.',
+    Name: "Leather",
+    Characteristic: "is a strong, durable, and wrinkle-resistant fabric.",
   },
   {
-    Name: 'Linen',
+    Name: "Linen",
     Characteristic:
-      'is breathable and naturally cool. It creases fairly easily.',
+      "is breathable and naturally cool. It creases fairly easily.",
   },
   {
-    Name: 'Lycra',
-    Characteristic:
-      "is elastic. It's blended with other fabric types to make them stretchy.",
-  },
-  {
-    Name: 'Lyocell',
-    Characteristic:
-      'is soft, breathable, light and can be stretchy. This makes it ideal for sportswear.',
-  },
-  {
-    Name: 'Modal',
-    Characteristic: 'is soft and strong pricier alternative to cotton.',
-  },
-  {
-    Name: 'Nylon',
-    Characteristic: 'is strong, light, and stretchy.',
-  },
-  {
-    Name: 'Polyamide',
-    Characteristic: 'is strong, light, and stretchy.',
-  },
-  {
-    Name: 'Polyester',
-    Characteristic:
-      'is durable and crease resistant. It can be reasonably stretchy and may feel sweaty when the weather is hot.',
-  },
-  {
-    Name: 'Polyurethane',
+    Name: "Lycra",
     Characteristic:
       "is elastic. It's blended with other fabric types to make them stretchy.",
   },
   {
-    Name: 'Rayon',
-    Characteristic: 'is lightweight, flowy, and doesn’t wrinkle easily.',
-  },
-  {
-    Name: 'Satin',
+    Name: "Lyocell",
     Characteristic:
-      'is soft, shiny and slightly elastic. It drapes so may not be form-fitting.',
+      "is soft, breathable, light and can be stretchy. This makes it ideal for sportswear.",
   },
   {
-    Name: 'Silk',
+    Name: "Mesh",
     Characteristic:
-      'is strong and durable but also very lightweight with a sheen. It drapes so may not be form-fitting.',
+      "is usually made from polyester, nylon or spandex finely formed into small holes.",
   },
   {
-    Name: 'Spandex',
+    Name: "Modal",
+    Characteristic: "is soft and strong pricier alternative to cotton.",
+  },
+  {
+    Name: "Nylon",
+    Characteristic: "is strong, light, and stretchy.",
+  },
+  {
+    Name: "Polyamide",
+    Characteristic: "is strong, light, and stretchy.",
+  },
+  {
+    Name: "Polyester",
+    Characteristic:
+      "is durable and crease resistant. It can be reasonably stretchy and may feel sweaty when the weather is hot.",
+  },
+  {
+    Name: "Polyurethane",
     Characteristic:
       "is elastic. It's blended with other fabric types to make them stretchy.",
   },
   {
-    Name: 'Suede',
-    Characteristic:
-      'is softer and thinner than traditional leather but is still very durable.',
+    Name: "Rayon",
+    Characteristic: "is lightweight, flowy, and doesn’t wrinkle easily.",
   },
   {
-    Name: 'Tencel',
+    Name: "Satin",
     Characteristic:
-      'is soft, breathable, light and can be stretchy. This makes it ideal for sportswear.',
+      "is soft, shiny and slightly elastic. It drapes so may not be form-fitting.",
   },
   {
-    Name: 'Velvet',
+    Name: "Silk",
     Characteristic:
-      'is soft and shiny but also has drape, making it suitable for dresses or even upholstery.',
+      "is strong and durable but also very lightweight with a sheen. It drapes so may not be form-fitting.",
   },
   {
-    Name: 'Viscose',
+    Name: "Spandex",
     Characteristic:
-      'has a similar drape and smooth feel to silk. It is lightweight and flowy.',
+      "is elastic. It's blended with other fabric types to make them stretchy.",
   },
   {
-    Name: 'Wool',
+    Name: "Suede",
     Characteristic:
-      'is soft and very warm. It has a little bit of stretch to it and it can be itchy if it is unlined.',
+      "is softer and thinner than traditional leather but is still very durable.",
+  },
+  {
+    Name: "Tencel",
+    Characteristic:
+      "is soft, breathable, light and can be stretchy. This makes it ideal for sportswear.",
+  },
+  {
+    Name: "Velvet",
+    Characteristic:
+      "is soft and shiny but also has drape, making it suitable for dresses or even upholstery.",
+  },
+  {
+    Name: "Viscose",
+    Characteristic:
+      "has a similar drape and smooth feel to silk. It is lightweight and flowy.",
+  },
+  {
+    Name: "Wool",
+    Characteristic:
+      "is soft and very warm. It has a little bit of stretch to it and it can be itchy if it is unlined.",
   },
 ];
 
 const config = {
   validAsosBrands: [
-    'ASOS',
-    'ASOS DESIGN',
-    'ASOS EDITION',
-    'ASOS WHITE',
-    'ASOS MADE IN',
-    'ASOS 4505',
-    'ASOS LUXE',
-    'Reclaimed Vintage',
-    'COLLUSION',
+    "ASOS",
+    "ASOS DESIGN",
+    "ASOS EDITION",
+    "ASOS WHITE",
+    "ASOS MADE IN",
+    "ASOS 4505",
+    "ASOS LUXE",
+    "Reclaimed Vintage",
+    "COLLUSION",
   ],
 
   brands: {
-    asos: 'ASOS',
-    bershka: 'Bershka',
-    boohoo: 'Boohoo',
-    hm: 'H&M',
-    iSawItFirst: 'I Saw It First',
-    mango: 'Mango',
-    missguided: 'Missguided',
-    monki: 'Monki',
-    nakd: 'NA-KD',
-    next: 'Next',
-    otherStories: 'OtherStories',
-    plt: 'PrettyLittleThing',
-    pullBear: 'Pull&Bear',
-    uniqlo: 'Uniqlo',
-    zara: 'Zara',
+    asos: "ASOS",
+    bershka: "Bershka",
+    boohoo: "Boohoo",
+    hm: "H&M",
+    houseOfCB: "House of CB",
+    iSawItFirst: "I Saw It First",
+    mango: "Mango",
+    massimoDutti: "Massimo Dutti",
+    missguided: "Missguided",
+    monki: "Monki",
+    ms: "M&S",
+    nakd: "NA-KD",
+    next: "Next",
+    otherStories: "OtherStories",
+    plt: "PrettyLittleThing",
+    pullBear: "Pull&Bear",
+    stradivarius: "Stradivarius",
+    uniqlo: "Uniqlo",
+    zara: "Zara",
   },
 
   categories: [
     {
-      name: 'One-pieces',
+      name: "One-pieces",
       keywords: [
-        'boiler suit',
-        'boilersuit',
-        'dress',
-        'dungaree',
-        'gown',
-        'jumpsuit',
-        'kimono',
-        'LBD',
-        'overall',
-        'pinafore',
-        'playsuit',
-        'sundress',
-        'unitard',
+        "boiler suit",
+        "boilersuit",
+        "dress",
+        "dungaree",
+        "gown",
+        "jumpsuit",
+        "kimono",
+        "LBD",
+        "overall",
+        "pinafore",
+        "playsuit",
+        "sundress",
+        "unitard",
       ],
     },
     {
-      name: 'Outerwear',
+      name: "Outerwear",
       keywords: [
-        'blazer',
-        'coat',
-        'fleece',
-        'gilet',
-        'hoodie',
-        'jacket',
-        'overshirt',
-        'overcoat',
-        'parka',
-        'poncho',
-        'puffer',
-        'raincoat',
-        'shacket',
-        'waistcoat',
-        'trench',
+        "blazer",
+        "coat",
+        "fleece",
+        "gilet",
+        "hoodie",
+        "jacket",
+        "overshirt",
+        "overcoat",
+        "parka",
+        "poncho",
+        "puffer",
+        "raincoat",
+        "shacket",
+        "waistcoat",
+        "trench",
       ],
     },
     {
-      name: 'Tops',
+      name: "Tops",
       keywords: [
-        'blouse',
-        'bodice',
-        'body',
-        'bodysuit',
-        'camisole',
-        'cardigan',
-        'chemise',
-        'dashiki',
-        'jumper',
-        'leotard',
-        'polo',
-        'shirt',
-        'shirtdress',
-        'sweater',
-        'sweatshirt',
-        'tunic',
-        't-shirt',
-        'top',
-        'vest',
+        "blouse",
+        "bodice",
+        "body",
+        "bodysuit",
+        "camisole",
+        "cardigan",
+        "chemise",
+        "corset",
+        "dashiki",
+        "jumper",
+        "leotard",
+        "polo",
+        "shirt",
+        "shirtdress",
+        "sweater",
+        "sweatshirt",
+        "t-shirt",
+        "top",
+        "tunic",
+        "vest",
       ],
     },
     {
-      name: 'Bottoms',
+      name: "Bottoms",
       keywords: [
-        'bell-bottom',
-        'capri',
-        'chino',
-        'culotte',
-        'flare',
-        'jean',
-        'jegging',
-        'Jodhpur',
-        'jogger',
-        'khakis',
-        'legging',
-        'miniskirt',
-        'sarong',
-        'short',
-        'skirt',
-        'skort',
-        'sport tight',
-        'sports tight',
-        'sweatpant',
-        'tracksuit',
-        'trouser',
-        'trunks',
-        'tutu',
+        "bell-bottom",
+        "capri",
+        "chino",
+        "culotte",
+        "flare",
+        "jean",
+        "jegging",
+        "Jodhpur",
+        "jogger",
+        "khakis",
+        "legging",
+        "miniskirt",
+        "sarong",
+        "short",
+        "skirt",
+        "skort",
+        "sport tight",
+        "sports tight",
+        "sweatpant",
+        "tracksuit",
+        "trouser",
+        "trunks",
+        "tutu",
       ],
     },
   ],
@@ -267,7 +281,7 @@ const getCategory = (product_name) => {
         category.keywords.filter((keyword) =>
           product_name.toLowerCase().includes(keyword)
         ).length
-    )[0]?.name || ''
+    )[0]?.name || ""
   );
 };
 
