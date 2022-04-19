e9e21b155b5ebc563098bfb2bc28502586e580cf
Adilson Bacelar <a.m.bacelar@gmail.com>
2022-04-19 19:39:26 +0100

    HouseofCB new arrivals

--- __tests__/newArrivals.test.js
+++ __tests__/newArrivals.test.js
@@ -1,113 +1,113 @@
-const { getCategory, findBrand } = require('../utils');
-require('expect-puppeteer');
+const { getCategory, findBrand } = require("../utils");
+require("expect-puppeteer");
 
 const validateItemNames = (itemNames, brandCheck = false) => {
   let brandCheckList = [];
-  let items = itemNames.filter((itemName) => getCategory(itemName) === '');
+  let items = itemNames.filter((itemName) => getCategory(itemName) === "");
   if (brandCheck) {
     brandCheckList = itemNames.filter((itemName) => !findBrand(itemName));
   }
   return [...items, ...brandCheckList];
 };
 
-describe('New items on H&M', () => {
+describe("New items on H&M", () => {
   it(
-    'should all pass through the test as capturable items',
+    "should all pass through the test as capturable items",
     async () => {
       await page.goto(
-        'https://www2.hm.com/en_gb/ladies/new-arrivals/view-all.html'
+        "https://www2.hm.com/en_gb/ladies/new-arrivals/view-all.html"
       );
       let itemNames = await page.evaluate(() => {
         const result = [];
-        const nodeArray = document.querySelectorAll('.item-details a.link');
+        const nodeArray = document.querySelectorAll(".item-details a.link");
         nodeArray.forEach((node) => result.push(node.innerText));
         return result;
       });
       expect(itemNames.length > 0).toEqual(true);
       itemNames = validateItemNames(itemNames);
-      if (itemNames.length !== 0) console.log('H&M list:', itemNames);
+      if (itemNames.length !== 0) console.log("H&M list:", itemNames);
       expect(true).toEqual(true);
     },
     30 * 1000
   );
 });
 
-describe('New items on Mango', () => {
+describe("New items on Mango", () => {
   it(
-    'should all pass through the test as capturable items',
+    "should all pass through the test as capturable items",
     async () => {
       await page.goto(
-        'https://shop.mango.com/gb/women/featured/new-now_d71927648'
+        "https://shop.mango.com/gb/women/featured/new-now_d71927648"
       );
       let itemNames = await page.evaluate(() => {
         const result = [];
-        const nodeArray = document.querySelectorAll('span.product-name');
+        const nodeArray = document.querySelectorAll("span.product-name");
         nodeArray.forEach((node) => result.push(node.innerText));
         return result;
       });
       expect(itemNames.length > 0).toEqual(true);
       itemNames = validateItemNames(itemNames);
-      if (itemNames.length !== 0) console.log('Mango list:', itemNames);
+      if (itemNames.length !== 0) console.log("Mango list:", itemNames);
       expect(true).toEqual(true);
     },
     30 * 1000
   );
 });
 
-describe('New items on Uniqlo', () => {
+describe("New items on Uniqlo", () => {
   it(
-    'should all pass through the test as capturable items',
+    "should all pass through the test as capturable items",
     async () => {
       await page.goto(
-        'https://www.uniqlo.com/uk/en/women/featured/new-arrivals'
+        "https://www.uniqlo.com/uk/en/women/featured/new-arrivals"
       );
       let itemNames = await page.evaluate(() => {
         const result = [];
         const nodeArray = document.querySelectorAll(
-          'span.productTile__heading'
+          "span.productTile__heading"
         );
         nodeArray.forEach((node) => result.push(node.innerText));
         return result;
       });
       expect(itemNames.length > 0).toEqual(true);
       itemNames = validateItemNames(itemNames);
-      if (itemNames.length !== 0) console.log('Uniqlo list:', itemNames);
+      if (itemNames.length !== 0) console.log("Uniqlo list:", itemNames);
       expect(true).toEqual(true);
     },
     30 * 1000
   );
 });
 
-describe('New items on Zara', () => {
+describe("New items on Zara", () => {
   it(
-    'should all pass through the test as capturable items',
+    "should all pass through the test as capturable items",
     async () => {
       await page.goto(
-        'https://www.zara.com/uk/en/woman-new-in-l1180.html?v1=2026572'
+        "https://www.zara.com/uk/en/woman-new-in-l1180.html?v1=2026572"
       );
       let itemNames = await page.evaluate(() => {
         const result = [];
         const nodeArray = document.querySelectorAll(
-          '.product-grid-product-info__main-info a.product-link._item.product-grid-product-info__name.link'
+          ".product-grid-product-info__main-info a.product-link._item.product-grid-product-info__name.link"
         );
         nodeArray.forEach((node) => result.push(node.innerText));
         return result;
       });
       expect(itemNames.length > 0).toEqual(true);
       itemNames = validateItemNames(itemNames);
-      if (itemNames.length !== 0) console.log('Zara list:', itemNames);
+      if (itemNames.length !== 0) console.log("Zara list:", itemNames);
       expect(true).toEqual(true);
     },
     30 * 1000
   );
 });
 
-describe('New items on Asos', () => {
+describe("New items on Asos", () => {
   it(
-    'should all pass through the test as capturable items',
+    "should all pass through the test as capturable items",
     async () => {
       await page.goto(
-        'https://www.asos.com/women/new-in/new-in-clothing/cat/?cid=2623&nlid=ww|new+in|new+products|clothing'
+        "https://www.asos.com/women/new-in/new-in-clothing/cat/?cid=2623&nlid=ww|new+in|new+products|clothing"
       );
       let itemNames = await page.evaluate(() => {
         const result = [];
@@ -119,7 +119,29 @@ describe('New items on Asos', () => {
       });
       expect(itemNames.length > 0).toEqual(true);
       itemNames = validateItemNames(itemNames, true);
-      if (itemNames.length !== 0) console.log('ASOS list:', itemNames);
+      if (itemNames.length !== 0) console.log("ASOS list:", itemNames);
+      expect(true).toEqual(true);
+    },
+    30 * 1000
+  );
+});
+
+describe("New items on House of CB", () => {
+  it(
+    "should all pass through the test as capturable items",
+    async () => {
+      await page.goto("https://www.houseofcb.com/new-arrivals.html");
+      let itemNames = await page.evaluate(() => {
+        const result = [];
+        const nodeArray = document.querySelectorAll(
+          ".category-products li div a.product-title"
+        );
+        nodeArray.forEach((node) => result.push(node.innerText));
+        return result;
+      });
+      expect(itemNames.length > 0).toEqual(true);
+      itemNames = validateItemNames(itemNames);
+      if (itemNames.length !== 0) console.log("House of CB list:", itemNames);
       expect(true).toEqual(true);
     },
     30 * 1000
